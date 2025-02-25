2e3531055937ec5fefc52a62f8fc135c06466958
Mohamed Sarhan <mohsarhan88@gmail.com>
2025-02-25 13:50:45 +0200

    test

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -39,10 +39,10 @@ export class NavbarComponent implements OnInit {
         const toggleButton = this.toggleButton;
         const body = document.getElementsByTagName('body')[0];
         setTimeout(function(){
-            toggleButton.classList.add('toggled');
+            toggleButton?.classList.add('toggled');
         }, 500);
 
-        body.classList.add('nav-open');
+        body?.classList.add('nav-open');
 
         this.sidebarVisible = true;
     };
@@ -50,7 +50,7 @@ export class NavbarComponent implements OnInit {
         const body = document.getElementsByTagName('body')[0];
         this.toggleButton?.classList.remove('toggled');
         this.sidebarVisible = false;
-        body.classList.remove('nav-open');
+        body?.classList.remove('nav-open');
     };
     sidebarToggle() {
         // const toggleButton = this.toggleButton;
@@ -66,19 +66,19 @@ export class NavbarComponent implements OnInit {
 
         if (this.mobile_menu_visible == 1) {
             // $('html').removeClass('nav-open');
-            body.classList.remove('nav-open');
+            body?.classList.remove('nav-open');
             var $layer = document.getElementsByClassName('close-layer')[0];
             if ($layer) {
                 $layer.remove();
             }
             setTimeout(function() {
-                $toggle.classList.remove('toggled');
+                $toggle?.classList.remove('toggled');
             }, 400);
 
             this.mobile_menu_visible = 0;
         } else {
             setTimeout(function() {
-                $toggle.classList.add('toggled');
+                $toggle?.classList.add('toggled');
             }, 430);
 
             var $layer2 = document.createElement('div');
@@ -87,25 +87,25 @@ export class NavbarComponent implements OnInit {
 
             if (body.querySelectorAll('.main-panel')) {
                 document.getElementsByClassName('main-panel')[0].appendChild($layer2);
-            }else if (body.classList.contains('off-canvas-sidebar')) {
+            }else if (body?.classList.contains('off-canvas-sidebar')) {
                 document.getElementsByClassName('wrapper-full-page')[0].appendChild($layer2);
             }
 
             setTimeout(function() {
-                $layer.classList.add('visible');
+                $layer?.classList.add('visible');
             }, 100);
 
             $layer2.onclick = function(this: NavbarComponent) { //asign a function
-              body.classList.remove('nav-open');
+              body?.classList.remove('nav-open');
               this.mobile_menu_visible = 0;
-              $layer.classList.remove('visible');
+              $layer?.classList.remove('visible');
               setTimeout(function() {
                   $layer.remove();
-                  $toggle.classList.remove('toggled');
+                  $toggle?.classList.remove('toggled');
               }, 400);
             }.bind(this);
 
-            body.classList.add('nav-open');
+            body?.classList.add('nav-open');
             this.mobile_menu_visible = 1;
 
         }
